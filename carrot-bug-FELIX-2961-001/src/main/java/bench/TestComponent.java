51a7e8e9b955113a92b29f8d5639253c16bf634b
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2011-06-05 10:48:59 -0500

    a

--- carrot-bug-FELIX-2961-001/src/main/java/bench/TestComponent.java
+++ carrot-bug-FELIX-2961-001/src/main/java/bench/TestComponent.java
@@ -8,6 +8,8 @@ import org.apache.felix.scr.annotations.Property;
  *
  * Description	Resource	Path	Location	Type
  The value for annotation attribute Component.name must be a constant expression	TestComponent.java	/carrot-bug-FELIX-2961-001/src/main/java/bench	line 5	Java Problem
+ *
+ * fine, this is how java annotations are now
  */
 // @Component(name = Name.PID)
 
@@ -20,5 +22,12 @@ public class TestComponent {
 	 */
 	@Property(name = "service.pid")
 	protected static final String PID = Name.PID;
+
+	/*
+	 * I would like the same magic on the following property; so, if present, it
+	 * should be applied to the <component> level, as if it was defined there
+	 */
+	@Property(name = "component.name")
+	protected static final String NAME = Name.PID;
 
 }
