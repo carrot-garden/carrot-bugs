e5b4831eb2f120006e7e66923e5435c5eb4b619a
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2011-06-05 10:39:38 -0500

    a

--- carrot-bug-FELIX-2961-001/launch/reproduce.sh
+++ carrot-bug-FELIX-2961-001/launch/reproduce.sh
@@ -2,11 +2,11 @@
 
 cd /tmp
 
-git clone https://github.com/carrot-garden/carrot-bugger
+git clone git://github.com/carrot-garden/carrot-bugger.git
 
-cd carrot-bug-FELIX-2961-001
+cd carrot-bugger/carrot-bug-FELIX-2961-001
 
-mvn clean package
+mvn clean package --define skipTests
 
 cat ./target/classes/OSGI-INF/serviceComponents.xml
 
