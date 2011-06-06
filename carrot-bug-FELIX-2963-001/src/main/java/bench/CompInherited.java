f27ef5097ad2d049a382447daa9077d45a01b11f
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2011-06-06 11:30:39 -0500

    a

--- carrot-bug-FELIX-2963-001/src/main/java/bench/CompInherited.java
+++ carrot-bug-FELIX-2963-001/src/main/java/bench/CompInherited.java
@@ -7,15 +7,15 @@ import org.apache.felix.scr.annotations.Component;
 @Component
 public class CompInherited extends CompBase {
 
-	protected final void activateDIRECT(final Map<String, String> config) {
+	protected final void activateINHERITED(final Map<String, String> config) {
 		activateBASE(config);
 	}
 
-	protected final void deactivateDIRECT(final Map<String, String> config) {
+	protected final void deactivateINHERITED(final Map<String, String> config) {
 		deactivateBASE(config);
 	}
 
-	protected final void modifiedDIRECT(final Map<String, String> config) {
+	protected final void modifiedINHERITED(final Map<String, String> config) {
 		modifiedBASE(config);
 	}
 
